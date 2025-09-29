6a43d3f42e582c106f333b26b548295fba653fa3
ChrisGit889 <christhiohermawan88@gmail.com>
2025-09-29 22:25:58 +0700

    awal

---
+++ data.js
@@ -0,0 +1,359 @@
+const dataWayang = [
+    {
+        judul: "Nakula",
+        pembuat: "Sukarman",
+        tanggalDibuat: "1986/1989",
+        jenis: ["Wayang Kulit"],
+        medium: "Kulit kerbau, Cat air dicampur perekat"
+    },
+    {
+        judul: "Singa Nila (Bali)",
+        pembuat: "Tidak dikenal",
+        tanggalDibuat: null,
+        jenis: ["Wayang Kulit", "Wayang"],
+        medium: "Kulit kerbau, Cat air dicampur perekat"
+    },
+    {
+        judul: "Raden Lesmono Widagdo (Surakarta)",
+        pembuat: "Tidak dikenal",
+        tanggalDibuat: null,
+        jenis: ["Wayang Kulit", "Wayang"],
+        medium: "Kulit kerbau, Cat air dicampur perekat"
+    },
+    {
+        judul: "Raden Brotoseno",
+        pembuat: "Tidak dikenal",
+        tanggalDibuat: null,
+        jenis: ["Wayang Kulit", "Wayang", "Boneka Kulit"],
+        medium: "Kulit kerbau, Cat air dicampur perekat"
+    },
+    {
+        judul: "Prabu Sasra",
+        pembuat: "Sukarman",
+        tanggalDibuat: null,
+        jenis: ["Boneka Kulit", "Wayang Kulit", "Wayang"],
+        medium: "Kulit kerbau, Cat air dicampur dengan perekat"
+    },
+    {
+        judul: "Arjuna",
+        pembuat: null,
+        tanggalDibuat: "2009",
+        jenis: ["Wayang Kulit"],
+        medium: "Kulit kerbau, Cat air dicampur perekat"
+    },
+    {
+        judul: "Raden Patah",
+        pembuat: "Suryadi Wano Surhardjo",
+        tanggalDibuat: "1975",
+        jenis: ["Wayang Kulit"],
+        medium: "Kulit kerbau, Cat air dicampur perekat",
+        genre: "Wayang Sadad"
+    },
+    {
+        judul: "Prabu Kresna (Cirebon)",
+        pembuat: "Tidak dikenal",
+        tanggalDibuat: null,
+        jenis: ["Wayang Kulit"],
+        medium: "Kulit kerbau, Cat air dicampur perekat"
+    },
+    {
+        judul: "Dursosono",
+        pembuat: "Tidak dikenal",
+        tanggalDibuat: null,
+        jenis: ["Wayang Kulit"],
+        medium: "Kulit kerbau, Cat air dicampur perekat"
+    },
+    {
+        judul: "Raja TIiam",
+        pembuat: "Tidak dikenal",
+        tanggalDibuat: null,
+        jenis: ["Wayang Kulit"],
+        medium: "Kulit kerbau, Cat air dicampur perekat"
+    },
+    {
+        judul: "Prabu Baladewa (Cirebon)",
+        pembuat: "Tidak dikenal",
+        tanggalDibuat: null,
+        jenis: ["Wayang Kulit", "Wayang"],
+        medium: "Kulit kerbau, Cat air dicampur perekat"
+    },
+    {
+        judul: "Romo Jangkah",
+        pembuat: "Sukarman",
+        tanggalDibuat: "1985, 1989",
+        jenis: ["Wayang Kulit"],
+        medium: "Kulit kerbau, Cat air dicampur perekat"
+    },
+    {
+        judul: "Pangeran Diponegoro",
+        pembuat: "Raden Mas Sayid",
+        tanggalDibuat: "1945/1950",
+        lokasi: "Jakarta, Indonesia, 6.2088,106.8456",
+        asal: "Diterima sebagai hadiah dari Wereldmuseum Rotterdam",
+        jenis: ["Boneka kulit", "Wayang kulit", "Wayang"],
+        medium: "Kulit kerbau, Cat air dicampur dengan perekat"
+    },
+    {
+        judul: "Punto Dewo",
+        pembuat: "Sukarman",
+        tanggalDibuat: "1986/1989",
+        lokasi: "Jakarta Indonesia",
+        jenis: ["Boneka Kulit", "Wayang", "Wayang Kulit"],
+        medium: "Kulit kerbau, Cat air dicampur dengan perekat"
+    },
+    {
+        judul: "Gatot Kaca",
+        pembuat: null,
+        tanggalDibuat: null,
+        lokasi: "Jakarta, Indonesia, -6.2088,106.8456",
+        jenis: ["Boneka Kulit", "Wayang", "Wayang Kulit", "Wayang kulit"],
+        medium: "Kulit kerbau, Cat air dicampur dengan perekat"
+    },
+    {
+        judul: "Patih Sangkuni",
+        pembuat: "Bambang Suwarno",
+        tanggalDibuat: "2014",
+        lokasi: "Jakarta",
+        jenis: ["Wayang kulit", "Boneka kulit", "Wayang", "Wayang Kulit"],
+        medium: "Kulit kerbau, Cat air dicampur dengan perekat"
+    },
+    {
+        judul: "Semar Lurah",
+        pembuat: "Saderi",
+        tanggalDibuat: "2010",
+        jenis: ["Wayang Kulit", "Wayang"],
+        medium: "Kulit kerbau, Cat air dicampur perekat"
+    },
+    {
+        judul: "Raden Bakhtiar",
+        pembuat: "Amak Rahimah",
+        tanggalDibuat: "1925",
+        jenis: ["Wayang Kulit", "Wayang"],
+        medium: "Kulit kerbau, Cat air dicampur perekat"
+    },
+    {
+        judul: "Hanuman",
+        pembuat: "Sukarman",
+        tanggalDibuat: "1985/1989",
+        jenis: ["Wayang", "Boneka kulit", "Wayang Kulit", "Wayang kulit"],
+        medium: "Kulit kerbau, Cat air dicampur dengan perekat"
+    },
+    {
+        judul: "Bratasena",
+        pembuat: "Niin Neran Ciracas",
+        tanggalDibuat: null,
+        lokasi: "Jakarta, Indonesia",
+        jenis: ["Boneka Kulit", "Wayang", "Wayang Kulit"],
+        medium: "Kulit kerbau, Cat air dicampur perekat"
+    },
+    {
+        judul: "Bala Dewa",
+        pembuat: "Tidak dikenal",
+        tanggalDibuat: null,
+        lokasi: "Jakarta, Indonesia",
+        jenis: ["Boneka kulit", "Wayang", "Wayang Kulit"],
+        medium: "Kulit kerbau, Cat air dicampur dengan perekat"
+    },
+    {
+        judul: "Anggada",
+        pembuat: "Sukasman",
+        tanggalDibuat: "1964",
+        jenis: ["Wayang Kulit", "Wayang"],
+        medium: "Kulit kerbau, Cat air dicampur perekat"
+    },
+    {
+        judul: "Gondo Mono",
+        pembuat: "Tidak dikenal",
+        tanggalDibuat: null,
+        jenis: ["Wayang Kulit", "Wayang"],
+        medium: "Kulit kerbau, Cat air dicampur perekat"
+    },
+    {
+        judul: "Raseksi",
+        pembuat: "Tidak dikenal",
+        tanggalDibuat: null,
+        jenis: ["Wayang Kulit", "Wayang"],
+        medium: "Kulit Kerbau, Cat air dicampur dengan perekat"
+    },
+    {
+        judul: "Duryudono",
+        pembuat: "Tidak dikenal",
+        tanggalDibuat: null,
+        jenis: ["Wayang Kulit"],
+        medium: "Kulit kerbau, Cat air dicampur perekat"
+    },
+    {
+        judul: "Janoko",
+        pembuat: "Tidak dikenal",
+        tanggalDibuat: null,
+        jenis: ["Boneka Kulit", "Wayang Kulit", "Wayang"],
+        medium: "Kulit Kerbau, Cat air dicampur dengan perekat"
+    },
+    {
+        judul: "Panji Inukertapati",
+        pembuat: "Tidak dikenal",
+        tanggalDibuat: null,
+        lokasi: "Jakarta, -6.2088,106.8456",
+        jenis: ["Boneka Kulit", "Wayang", "Wayang Kulit"],
+        medium: "Kulit kerbau, Cat air dicampur dengan perekat"
+    },
+    {
+        judul: "Prabu Alam Dahur",
+        pembuat: "Amak Rahimah",
+        tanggalDibuat: "1925",
+        lokasi: "Jakarta, Indonesia",
+        jenis: ["Wayang", "Wayang Kulit", "Boneka Kulit"],
+        medium: "Kulit kerbau, Cat air dicampur perekat"
+    },
+    {
+        judul: "Tugu Waseso",
+        pembuat: "Tidak dikenal",
+        tanggalDibuat: null,
+        jenis: ["Wayang Kulit"],
+        medium: "Kulit kerbau, Cat air dicampur perekat"
+    },
+    {
+        judul: "Bagong",
+        pembuat: "Saderi",
+        tanggalDibuat: "2010",
+        jenis: ["Wayang Kulit", "Wayang"],
+        medium: "Kulit kerbau, Cat air dicampur perekat"
+    },
+    {
+        judul: "Raden Haryo MIngdo",
+        pembuat: ["Pringgo", "Mardi", "Jarot"],
+        tanggalDibuat: "1988/1992",
+        jenis: ["Wayang Kulit", "Wayang"],
+        medium: "Kulit kerbau, Cat air dicampur perekat"
+    },
+    {
+        judul: "Lasmana Dikomara",
+        pembuat: null,
+        tanggalDibuat: null,
+        lokasi: "Jakarta, Indonesia",
+        jenis: ["Boneka kulit", "Wayang", "Wayang kulit", "Wayang Kulit"],
+        medium: "Kulit kerbau, Car air dicampur perekat"
+    },
+    {
+        judul: "Bima Sena",
+        pembuat: "Sukarman",
+        tanggalDibuat: "2009",
+        jenis: ["Boneka Kulit", "Wayang", "Wayang Kulit"],
+        medium: "Kulit kerbau, Cat air dicampur perekat"
+    },
+    {
+        judul: "Raden Werkudoro",
+        pembuat: "Bambang Suwarno",
+        tanggalDibuat: "2014",
+        jenis: ["Wayang Kulit", "Wayang"],
+        medium: "Kulit kerbau, Cat air dicampur perekat"
+    },
+    {
+        judul: "Raden Danur Wendo",
+        pembuat: null,
+        tanggalDibuat: null,
+        jenis: ["Wayang", "Boneka Kulit", "Wayang Kulit"],
+        medium: "Kulit kerbau, Cat air dicampur dengan perekat"
+    },
+    {
+        judul: "Pratsongko",
+        pembuat: "Tidak dikenal",
+        tanggalDibuat: null,
+        jenis: ["Wayang Kulit"],
+        medium: "Kulit kerbau, Cat air dicampur perekat"
+    },
+    {
+        judul: "Prabu Rahwana",
+        pembuat: "Mbah Karmo",
+        tanggalDibuat: null,
+        jenis: ["Wayang Kulit", "Wayang"],
+        medium: "Kulit kerbau, Cat air dicampur perekat",
+        genre: "Wayang Palembang"
+    },
+    {
+        judul: "Onterja",
+        pembuat: "Tidak dikenal",
+        tanggalDibuat: null,
+        jenis: ["Boneka Kulit", "Wayang Kulit", "Wayang"],
+        medium: "Kulit Kerbau, Cat air dicampur perekat"
+    },
+    {
+        judul: "Gangge Mine",
+        pembuat: "Amak Rahimah",
+        tanggalDibuat: null,
+        jenis: ["Wayang Kulit"],
+        medium: "Kulit kerbau, Cat air dicampur perekat",
+        genre: "Wayang Sasak"
+    },
+    {
+        judul: "Bisma",
+        pembuat: "Tidak dikenal",
+        tanggalDibuat: null,
+        jenis: ["Wayang Kulit", "Wayang"],
+        medium: "Kulit kerbau, Cat air dicampur perekat"
+    },
+    {
+        judul: "Prabu Brawijaya",
+        pembuat: "Suryadi Wanosuhardjo",
+        tanggalDibuat: "1975",
+        jenis: ["Wayang Kulit"],
+        medium: "Kulit kerbau, Cat air dicampur perekat",
+        genre: "Wayang Sadad"
+    },
+    {
+        judul: "Sayid Ibnu Umar",
+        pembuat: "Amak Rahimah",
+        tanggalDibuat: "1925",
+        jenis: ["Wayang Kulit", "Wayang"],
+        medium: "Kulit kerbau, Cat air dicampur perekat"
+    },
+    {
+        judul: "Nala Gareng",
+        pembuat: "Saderi",
+        tanggalDibuat: "2010",
+        jenis: ["Wayang Kulit", "Wayang"],
+        medium: "Kulit kerbau, Cat air dicampur perekat"
+    },
+    {
+        judul: "Rahwana",
+        pembuat: "Tidak dikenal",
+        tanggalDibuat: null,
+        jenis: ["Wayang Kulit", "Wayang"],
+        medium: "Kulit kerbau, Cat air dicampur perekat"
+    },
+    {
+        judul: "Dewi Sinta",
+        pembuat: "Tidak dikenal",
+        tanggalDibuat: null,
+        jenis: ["Wayang Kulit", "Wayang"],
+        medium: "Kulit kerbau, Cat air dicampur perekat"
+    },
+    {
+        judul: "Hanoman",
+        pembuat: null,
+        tanggalDibuat: null,
+        jenis: ["Wayang Kulit", "Wayang"],
+        medium: "Kulit kerbau, Cat air dicampur perekat"
+    },
+    {
+        judul: "Sri Rama",
+        pembuat: "Tidak dikenal",
+        tanggalDibuat: null,
+        jenis: ["Wayang Kulit", "Wayang"],
+        medium: "Kulit kerbau, Cat air dicampur perekat"
+    },
+    {
+        judul: "Erang Ping",
+        pembuat: "Gan Thwan Sing",
+        tanggalDibuat: "2005",
+        jenis: ["Wayang Kulit", "Wayang"],
+        medium: "Kulit kerbau, Cat air dicampur perekat"
+    },
+    {
+        judul: "Umbu Karna",
+        pembuat: "Mbah Karmo",
+        tanggalDibuat: null,
+        jenis: ["Wayang Kulit", "Wayang"],
+        medium: "Kulit kerbau, Cat air dicampur perekat"
+    }
+];
